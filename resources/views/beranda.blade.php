f7baec3223864e2569ce28f7bfe7f4932d2e8d38
afaafa <afhazachra@gmail.com>
2021-01-11 12:16:09 +0700

    install system login

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,12 +1,12 @@
-@extends('template')
+@extends('layouts.app')
 
 @section('content')
 
     <div class="jumbotron text-center">
         <h1>Portal Informasi Siswa</h1>
         <p>Selamat Datang di Portal Informasi Siswa SMA 404</p>
-        <a class="btn btn-dark" href="#">Info Kegiatan</a>
-        <a class="btn btn-primary" href="#">Data Siswa</a>
+        <a class="btn btn-dark" href="{{ url('/info-kegiatan') }}">Info Kegiatan</a>
+        <a class="btn btn-primary" href="{{ url('/data-siswa') }}">Data Siswa</a>
     </div>
 
 
