768373145cfd4a42d9f0b53dd58dccf5fabac9c6
afaafa <afhazachra@gmail.com>
2021-01-13 19:23:33 +0700

    edit title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SMA 404') }}</title>
+    <!--<title>{{ config('app.name', 'SMA 404') }}</title>-->
+    <title>SMA 404</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
